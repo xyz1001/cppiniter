e3c38ae7d2397542b0659e7bba8aeba00daa5754
xyz1001 <zgzf1001@gmail.com>
2017-11-07 23:05:41 +0800

    [refactor] 调整代码结构

--- cppiniter.py
+++ cppiniter.py
@@ -18,10 +18,10 @@ import shutil
 import json
 import sys
 import pystache
-from datetime import datetime
+import time
 from docopt import docopt
 
-cppiniter_dir = ""
+DATA_INSTALL_DIR = "/usr/local/share/cppiniter"
 VERSION = "1.0"
 TMP_DIR = "/tmp/cppiniter"
 
@@ -31,7 +31,7 @@ class Project(object):
         self.name = project_name
         self.build_system = build_system
         self.is_lib = is_lib
-        self.data_file_dir = os.path.join(cppiniter_dir, "data", self.build_system)
+
 
     def __str__(self):
         return "name: %s\ndir: %s\nbuild system: %s\nlibrary: %s"%(self.dir,self.name, self.build_system, self.is_lib)
@@ -39,24 +39,81 @@ class Project(object):
     def __repr__(self):
         return self.__str__()
 
-def checkEnv(project_dir):
-    cppiniter_dir = os.path.split(os.path.realpath(__file__))[0]
-    if not os.path.isdir(os.path.join(cppiniter_dir, "data")):
-        cppiniter_dir = "/usr/local/share/cppiniter"
-        if not os.path.isdir(os.path.join(cppiniter_dir, "data")):
-            print("[Error]: 程序运行所需文件丢失，请尝试重新安装")
-            exit(1)
-    if not os.path.isdir(TMP_DIR):
-        os.mkdir(TMP_DIR)
-    if not os.path.isdir(project_dir):
-        os.mkdir(project_dir)
-    if os.listdir(project_dir):
-        ok = input("目标文件夹%s不为空，删除该文件夹下所有内容并继续？（y/N）"%project_dir)
-        if ok == "y" or ok == "Y":
-            shutil.rmtree(project_dir)
-            os.mkdir(project_dir)
+class Generator(object):
+    def generate(self, project):
+        self.project = project
 
-def parseArgs(args):
+        self.__check_env()
+        self.__copy_files()
+        self.__generate_files()
+        self.__create_file_tree()
+        self.__clean()
+
+    def __file_dir(self):
+        return os.path.join(self.data_dir, self.project.build_system)
+
+    def __check_env(self):
+        bin_dir = os.path.split(os.path.realpath(__file__))[0]
+        self.data_dir = os.path.join(bin_dir, "data")
+        if not os.path.isdir(self.data_dir):
+            self.data_dir = DATA_INSTALL_DIR
+            if not os.path.isdir(self.__file_dir()):
+                print("[Error]: 程序运行所需文件丢失，请尝试重新安装")
+                exit(1)
+        if not os.path.isdir(TMP_DIR):
+            os.mkdir(TMP_DIR)
+        if not os.path.isdir(self.project.dir):
+            os.mkdir(self.project.dir)
+        if os.listdir(self.project.dir):
+            ok = input("目标文件夹%s不为空，删除该文件夹下所有内容并继续？（y/N）"%self.project.dir)
+            if ok == "y" or ok == "Y":
+                shutil.rmtree(self.project.dir)
+                os.mkdir(self.project.dir)
+            else:
+                exit(-1)
+
+    def __generate_files(self) :
+        sys.path.append(self.__file_dir())
+        import template
+
+        info = {"project_dir": self.project.dir,
+                "project_name":self.project.name,
+                "build_system":self.project.build_system,
+                "is_lib":self.project.is_lib,
+                "date_time":str(time.strftime("%Y-%m-%d %A %X %Z",
+                    time.localtime())),
+                "version": VERSION};
+        for (k,v) in template.FILES.items():
+            content = pystache.render(v, info)
+            with open(os.path.join(TMP_DIR,k), "w") as fout:
+                    fout.write(content)
+
+    def __copy_files(self) :
+        files_dir = os.path.join(self.__file_dir(),"files")
+        for i in os.listdir(files_dir):
+            shutil.copy2(os.path.join(files_dir, i), os.path.join(TMP_DIR, i))
+
+    def __create_file_tree(self):
+        file_tree_path = os.path.join(self.__file_dir(),  "file_tree.json")
+        with open(file_tree_path, "r") as fin:
+            file_tree = json.loads(fin.read())
+        self.__create_tree(file_tree, self.project.dir)
+
+    def __create_tree(self, file_tree, dest_dir):
+        for (k, v) in file_tree.items():
+            if k != "file":
+                dir_path = os.path.join(dest_dir, k)
+                os.mkdir(dir_path)
+                self.__create_tree(v, dir_path)
+            else:
+                for (file_name,file_source) in v.items():
+                    shutil.copy2(os.path.join(TMP_DIR, file_source), os.path.join(dest_dir, file_name))
+
+    def __clean(self):
+        shutil.rmtree(TMP_DIR)
+
+
+def parse_args(args):
     project_dir = args["<dir>"]
     project_name = args["--name"]
     if args["--qmake"]:
@@ -72,51 +129,11 @@ def parseArgs(args):
         project_name = os.path.split(project_dir)[-1]
     return Project(project_dir, project_name, build_system, is_lib)
 
-def generateFiles(project) :
-    sys.path.append(project.data_file_dir)
-    import template
-
-    info = {"project_dir": project.dir,
-            "project_name":project.name,
-            "build_system":project.build_system,
-            "is_lib":project.is_lib,
-            "date_time":str(datetime.now()),
-            "version": VERSION};
-    for (k,v) in template.FILES.items():
-        content = pystache.render(v, info)
-        with open(os.path.join(TMP_DIR,k), "w") as fout:
-                fout.write(content)
-
-def copyFiles(project) :
-    files_dir = os.path.join(project.data_file_dir,"files")
-    for i in os.listdir(files_dir):
-        shutil.copy2(os.path.join(files_dir, i), os.path.join(TMP_DIR, i))
-
-def ParseFileTree(project):
-    file_tree_path = os.path.join(project.data_file_dir,  "file_tree.json")
-    with open(file_tree_path, "r") as fin:
-        file_tree = json.loads(fin.read())
-    return file_tree
-
-def CreateTree(file_tree, dest_dir):
-    for (k, v) in file_tree.items():
-        if k != "file":
-            dir_path = os.path.join(dest_dir, k)
-            os.mkdir(dir_path)
-            CreateTree(v, dir_path)
-        else:
-            for (file_name,file_source) in v.items():
-                shutil.copy2(os.path.join(TMP_DIR, file_source), os.path.join(dest_dir, file_name))
-
-def Clean():
-    shutil.rmtree(TMP_DIR)
+def main():
+    args = docopt(__doc__, version="1.0")
+    project = parse_args(args)
+    gen = Generator()
+    gen.generate(project);
 
 if __name__ == "__main__":
-    args = docopt(__doc__, version="1.0")
-    project = parseArgs(args)
-    checkEnv(project.dir)
-    copyFiles(project)
-    generateFiles(project)
-    file_tree = ParseFileTree(project)
-    CreateTree(file_tree, project.dir)
-    Clean()
+    main()
